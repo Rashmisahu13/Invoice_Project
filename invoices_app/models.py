cf65732a68878586995eb645e7721c75f5be527f
Rashmisahu13 <nikkisahu048@gmail.com>
2024-02-26 17:01:53 +0530

    second commit

--- invoices_app/models.py
+++ invoices_app/models.py
@@ -7,6 +7,6 @@ class Invoice(models.Model):
 class InvoiceDetail(models.Model):
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='details')
     description = models.CharField(max_length=255)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
